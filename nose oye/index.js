9f2f2376bb2276d418f5c730b7074d72dd3d66ce
idaira <idairaconejillo@gmail.com>
2024-04-04 16:51:04 +0200

    parriba mis reye mi ejercicio pilla poligoneros

---
+++ nose oye/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('se ta metio un poligonero aki');
+    socket.on('disconnect', () => {
+      console.log('poligonero EJEKUTAO');
+    });
+  });
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
