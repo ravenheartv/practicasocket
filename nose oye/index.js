e0ab07bfa079e756342d1a6a14bf6ccf97aa3828
idaira <idairaconejillo@gmail.com>
2024-04-11 16:18:55 +0200

    cambios wenos

--- nose oye/index.js
+++ nose oye/index.js
@@ -11,17 +11,26 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
+app.get('/paint', (req, res) => {
+  res.sendFile(join(__dirname, 'paint.html'));
+});
+
 io.on('connection', (socket) => {
     console.log('se ta metio un poligonero aki');
     socket.on('disconnect', () => {
       console.log('poligonero EJEKUTAO');
     });
-  });
-  io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
+    socket.on('paint', (datos) => {
+      console.log(datos);
+      io.emit('paint', datos);
+
+    });
+
   });
+
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
